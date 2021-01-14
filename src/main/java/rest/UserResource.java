17f075029ad95672781bb8ed138cf85750562295
LasseBirk96 <56427491+LasseBirk96@users.noreply.github.com>
2021-01-14 12:45:45 +0100

    Testing1

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -7,20 +7,16 @@ import entities.User;
 import errorhandling.API_Exception;
 import facades.UserFacade;
 import java.util.List;
-import javax.annotation.security.RolesAllowed;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 import security.errorhandling.AuthenticationException;
 import utils.EMF_Creator;
 
@@ -30,12 +26,11 @@ public class UserResource {
     
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
     public static final UserFacade USER_FACADE = UserFacade.getUserFacade(EMF);
-    
+  
     @Context
     private UriInfo context;
 
-    @Context
-    SecurityContext securityContext;
+
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
